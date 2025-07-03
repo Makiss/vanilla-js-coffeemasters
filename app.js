a5d492f6d6342016003013a60fa9c520b46b707f
Maksym Somushkin <syomushkin.maxim@gmail.com>
2025-07-03 15:46:49 +0200

    feat: fetch and store menu

---
+++ app.js
@@ -0,0 +1,10 @@
+import { loadData } from "./services/Menu.js";
+import Store from "./services/Store.js";
+
+window.app = {};
+app.store = Store;
+
+window.addEventListener("DOMContentLoaded", async function () {
+  await loadData();
+  console.log(app.store.menu);
+});
